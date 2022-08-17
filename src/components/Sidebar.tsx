57787c3295e22be2e832e26e4a001e361b7e244f
Emerson Lima <emerson.dev7@gmail.com>
2022-08-16 22:06:01 -0300

    sidebar component creation

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,24 @@
+import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
+
+export function Sidebar(){
+  return (
+        <aside className={styles.sidebar}>
+          <img 
+          src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+          className={styles.cover}
+          />
+
+          <div className={styles.profile}>
+            <img 
+              src="https://github.com/emersonlimaa.png"
+              className={styles.avatar}
+            />
+            <strong>Emerson Lima</strong>
+            <span>Web Developer</span>
+            <footer>
+              <a href="#"><PencilLine size={20}/> Editar seu perfil</a>
+            </footer>
+          </div>
+        </aside>)
+}

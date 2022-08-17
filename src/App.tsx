9938b9cffda05773eb1859fc9abf49c1cba21064
Emerson Lima <emerson.dev7@gmail.com>
2022-08-16 22:07:00 -0300

    creation app end style global

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import { useState } from 'react'
+import { Header } from './components/Header'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
+
+import './global.css'
+import styles from './App.module.css'
 
 function App() {
   return (
-    <h1></h1>
+    <div>
+      <Header/>
+      <div className={styles.wrapper}>
+      <Sidebar/>
+      <main>
+        <Post />
+        
+      </main>
+      </div>
+    </div>
   )
 }
 

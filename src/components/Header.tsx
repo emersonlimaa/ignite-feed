3135a5adcd7ed5c79b7f95c24255caa7f29c8bdc
Emerson Lima <emerson.dev7@gmail.com>
2022-08-16 22:06:25 -0300

    header component creation

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import Logo from '../assets/ignite-logo.svg'
+import styles from './Header.module.css'
+
+export function Header(){
+  return(
+    <header className={styles.header}>
+      <img src={Logo} alt="Logotipo Ignite" />
+      <strong>Ignite Feed</strong>
+    </header>
+    
+  )
+}

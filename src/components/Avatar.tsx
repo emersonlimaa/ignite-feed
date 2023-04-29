78e136a761a7186d9de1d3057467b3d722f76d00
Emerson Lima <emerson.dev7@gmail.com>
2023-04-29 18:11:59 -0300

    fix bug in attributes in Img

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,15 +1,15 @@
+import { ImgHTMLAttributes } from 'react'
 import styles from './Avatar.module.css'
 
-interface AvatarProps{
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{
   image: string,
   hasBorder?: boolean
-  alt?: string
 }
-export function Avatar({image, hasBorder, alt}: AvatarProps) {
+export function Avatar({hasBorder =true, ...props}: AvatarProps) {
   return(
-    <img 
+    <img  
       className={hasBorder ? styles.avatar : styles.avatarWithBorder} 
-      src={image} 
-      alt={alt} />
+      {...props}
+      />
   )
 }
